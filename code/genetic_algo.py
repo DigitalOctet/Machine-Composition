68e8223273cae2d69de1076cb9f67805e312785f
cccfl <cflyuke@gmail.com>
2024-12-22 18:03:38 +0800

    final

--- code/genetic_algo.py
+++ code/genetic_algo.py
@@ -2,7 +2,7 @@ from population import Population
 import fitness_test 
 if __name__ == "__main__":
     population = Population()
-    population.evolve(crossover='random')
+    population.evolve(crossover='routellet')
     best_clip = population.get_best_clip()
     best_clip.generate_wave()
     best_clip.generate_midi()
